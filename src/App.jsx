7545c6fb77bb35eff6b875f1702ed3fdcea09ab3
AnthonySultanov <114832556+AnthonySultanov@users.noreply.github.com>
2023-03-06 23:02:23 +0000

    anthony
    added homepage with working api shows games

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import {Home} from './components/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 
 
@@ -10,13 +14,12 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className='App'>
+        <div className='app'>
         <Navbar bg="dark" variant="dark">
           <Container>
           <Navbar.Brand href="/">MyShowList</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/home">Home</Nav.Link>
-              
             </Nav>
           </Container>
         </Navbar>
--- src/components/home.js
+++ src/components/home.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
-export class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getGames = this.getGames.bind(this);
-  }
+export function Home(props) {
+  const [games, setGames] = useState([]);
 
-  getGames() {
+  useEffect(() => {
     axios.get('https://api.rawg.io/api/games?key=c7400ff635084895be4319a6edaf857e&dates=2019-09-01,2019-09-30&platforms=18,1,7')
       .then(response => {
-        console.log(response);
+        setGames(response.data.results);
       });
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        <button onClick={this.getGames}>Home</button>
-      </div>
-    );
-  }
+  }, []);
+  
+  return (
+    <div className='App'>
+      <Card>
+        {/* <Card.Header>{props.results}</Card.Header> */}
+        
+        <Card.Body className='cardbody'>
+          {games.map(game => (
+            <Card key={game.id}>
+              <Card.Title>{game.name}</Card.Title>
+              <Card.Img className='gamesbannerimage' src={game.background_image}/>
+              <Card.Text>{game.released}</Card.Text>
+              <Card.Text>{game.rating}</Card.Text>
+            </Card>
+          ))}
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Home;
