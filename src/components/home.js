1272e966cf3acbb6ef201672c52d84ff09149d7e
AnthonySultanov <114832556+AnthonySultanov@users.noreply.github.com>
2023-05-18 04:51:28 +0100

    Add files via upload

--- src/components/home.js
+++ src/components/home.js
@@ -3,38 +3,38 @@ import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import GameContext from './GameContext';
-import './home.css';
-
-
-
+import { AuthContext } from './AuthContext'; // Import AuthContext
 
 export function Home() {
   const [games, setGames] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [addedGames, setAddedGames] = useState([]); // New
+  const [addedGames, setAddedGames] = useState([]);
+  
+  const { email } = useContext(AuthContext); // Access the email from AuthContext
 
   const handleAddClick = (game) => {
     const rating = prompt(`Rate ${game.name} out of 10?`);
     console.log(`You rated ${game.name} ${rating}/10`);
-
-    const profilegame = { name: game.name, rating: rating, background_image: game.background_image };
-
-    axios.post('http://localhost:5000/api/profile', {
-  name: game.name,
-  rating: rating,
-  background_image: game.background_image
-}, {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-
-
-
-    setAddedGames(addedGames => [...addedGames, profilegame]);
-  };
-
   
+    const profileGame = {
+      name: game.name,
+      rating: rating,
+      background_image: game.background_image
+    };
+  
+    axios.post('http://localhost:5000/api/profile', profileGame, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the request header
+      }
+    })
+    .then(response => {
+      setAddedGames(addedGames => [...addedGames, response.data.game]); // Update addedGames state with the newly added game
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  };
   
 
   useEffect(() => {
@@ -53,34 +53,32 @@ export function Home() {
 
   return (
     <GameContext.Provider value={[addedGames, setAddedGames]}>
-    <div className='App'>
-      <input
-        type='text'
-        placeholder='Search for a game'
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-     
-      <GameContext.Provider value={[addedGames, setAddedGames]}> {/* Updated */}
-        <Card>
-          <Card.Body className='cardbody'>
-            {games.map(game => (
-              <Card key={game.id}>
-                <Card.Title>{game.name}</Card.Title>
-                <Card.Img className='gamesbannerimage' src={game.background_image}/>
-                <Card.Text>{game.released}</Card.Text>
-                <Card.Text>{game.metacritic}</Card.Text>
-                <Button variant="danger" style={{ marginLeft: "auto" }} onClick={() => handleAddClick(game)}>Add</Button>
-              </Card>
-            ))}
-          </Card.Body>
-        </Card>
-      </GameContext.Provider> {/* Updated */}
-    </div>
+      <div className='App'>
+        <input
+          type='text'
+          placeholder='Search for a game'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+
+        <GameContext.Provider value={[addedGames, setAddedGames]}>
+          <Card>
+            <Card.Body className='cardbody'>
+              {games.map(game => (
+                <Card key={game.id}>
+                  <Card.Title>{game.name}</Card.Title>
+                  <Card.Img className='gamesbannerimage' src={game.background_image} />
+                  <Card.Text>Release Date: {game.released}</Card.Text>
+                  <Card.Text>Metacritic Rating: {game.metacritic}</Card.Text>
+                  <Button variant="danger" style={{ marginLeft: "auto" }} onClick={() => handleAddClick(game)}>Add</Button>
+                </Card>
+              ))}
+            </Card.Body>
+          </Card>
+        </GameContext.Provider>
+      </div>
     </GameContext.Provider>
   );
 }
-
-
 
 export default Home;
